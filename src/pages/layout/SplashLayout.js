b1135732e5549985c262838c33a5048c7e5e21f5
marxlow <marx.low@agolo.com>
2018-03-03 16:28:42 -0500

    Draft of splash page

--- src/pages/layout/SplashLayout.js
+++ src/pages/layout/SplashLayout.js
@@ -1,27 +1,37 @@
-//@flow
+// @flow
 
 import { Link, Route } from 'react-router';
 import React, { Component } from 'react';
 
-class SplashLayout extends Component {
-    constructor(props) {
-        super(props);
-    }
+import IntroCard from '../../components/Cards/IntroCard';
+import ProfileImg from '../../images/upstate_headshot.jpg';
 
-    render() {
-        return (
-            <div className="bg-gradient-info">
-                <header>
-                    <p> Header for Splash </p>
-                    <button type="button" className="btn btn-primary">Primary</button>
-                </header>
-                <div>
-                </div>
-                <footer>
-                </footer>
-            </div>
-        );
-    }
+class SplashLayout extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const name = "Marx Low";
+    const aboutMe = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis congue quam et sodales. Phasellus ac lacus sapien. Mauris venenatis.";
+    const socialMedia = [
+      { name: "FaceBook", url: "https://FacebookURL" },
+      { name: "Twitter", url: "https://TwitterURL" },
+      { name: "Instagram", url: "https://InstagramURL" },
+      { name: "Github", url: "https://GithubURL" },
+      { name: "LinkedIn", url: "https://linkedInURL" },
+    ]
+    const profileImg = "ProfileImg";
+    return (
+      <div className="splash-layout">
+        <IntroCard
+          name={name}
+          aboutMe={aboutMe}
+          socialMedia={socialMedia}
+          profileImg={ProfileImg}
+        />
+      </div>
+    );
+  }
 }
 
 export default SplashLayout;
